16867081cc3b7bd812242c3d42fe05dd00d3ec20
danieljonesdmj <danieljonesdmj@gmail.com>
2018-07-04 14:47:13 +0100

    setup test database

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,10 +3,13 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://google.com');")
+
+
       bookmarks = Bookmark.all
-      expect(bookmarks).to include('http://makersacademy.com')
       expect(bookmarks).to include('http://google.com')
-      expect(bookmarks).to include('http://destroyallsoftware.com')
     end
   end
 end
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,8 +1,10 @@
 feature 'view bookmarks' do
   scenario 'view bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://google.com');")
+
     visit('/bookmarks')
-    expect(page).to have_content 'http://makersacademy.com'
     expect(page).to have_content 'http://google.com'
-    expect(page).to have_content 'http://destroyallsoftware.com'
   end
 end
