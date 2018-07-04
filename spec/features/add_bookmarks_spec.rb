5f8ea97f1735201be477fcf532ffca91cb263fb2
Cornelis Heuperman <cheuperman@gmail.com>
2018-07-04 15:39:00 +0100

    Add test for guard against invalid entries

--- spec/features/add_bookmarks_spec.rb
+++ spec/features/add_bookmarks_spec.rb
@@ -5,4 +5,10 @@ feature 'Add bookmarks' do
     click_button('Add bookmark')
     expect(page).to have_content 'http://makersacademy.com'
   end
+  scenario 'invalid entry' do
+    visit '/'
+    fill_in('url', with: 'not a bookmark')
+    click_button('Add bookmark')
+    expect(page).to have_content 'Cannot add: not a valid URL'
+  end
 end
